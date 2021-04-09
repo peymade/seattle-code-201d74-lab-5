6e041a9c07c666900ad0bf0ad6538a9bfc69cc21
Peyton Mader <middpeyton@gmail.com>
2021-04-09 15:29:40 -0700

    Complete coding challenge 2

--- starterCode/app.js
+++ starterCode/app.js
@@ -10,8 +10,8 @@ function sum(a, b) { //eslint-disable-line
   let sumOfNumbers = a + b;
 // returning an array
 // [sumOfNumbers, 'The sum of ' +a+' and ' +b+' is ' + sumOfNumbers +'.']
-  let myArray = [sumOfNumbers, 'The sum of ' +a+' and ' +b+' is ' + sumOfNumbers +'.']
-  return myArray;
+  let sumArray = [sumOfNumbers, 'The sum of ' +a+' and ' +b+' is ' + sumOfNumbers +'.']
+  return sumArray;
 }
 
 // let result = sum(3, 10) if I just want the number I need to figure out how to get the 0 position
@@ -30,11 +30,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+  let productOfNumbers = a * b;
+  let productArray = [productOfNumbers, 'The product of ' +a+' and ' +b+' is ' + productOfNumbers +'.'];
+  return productArray;
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
